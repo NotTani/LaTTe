bce71a3ec1c3c34ad256c8ce316f90343196b4bd
Tani N-K <62847558+NotTani@users.noreply.github.com>
2021-04-23 14:00:37 -0400

    Change blockconverter API

--- latte/base/blockconverter.py
+++ latte/base/blockconverter.py
@@ -1,8 +1,21 @@
 # Block Converter Processor
 import abc
 
-
-class BlockConverter(abc.ABC):
+class BlockConverter:
     BLOCK_NAME = None
 
-    def convertEmbeddedHTML(self, parameters, body):
+    BLOCK_LEVEL = True
+    INLINE = True
+
+    def __init__(self, body, embedded=False, **args):
+        self.body = body
+        self.embedded = embedded
+        self.args = args
+
+    def __str__(self):
+        return f"<{self.__class__.__name__} {self.args}>"
+
+    def convertHTML(self, body, embedded=False, **args):
+        raise NotImplementedError(
+            f"Block {self.__class__.__name__} doesn't have a convertHTML method"
+        )
